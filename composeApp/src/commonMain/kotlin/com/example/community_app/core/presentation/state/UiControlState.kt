f437cf61eba451f99087595d7efa584756bc90a3
msuess <moritz.suess@hof-university.de>
2025-12-16 06:31:14 +0100

    Global helper states

--- composeApp/src/commonMain/kotlin/com/example/community_app/core/presentation/state/UiControlState.kt
+++ composeApp/src/commonMain/kotlin/com/example/community_app/core/presentation/state/UiControlState.kt
@@ -1,2 +1,9 @@
 package com.example.community_app.core.presentation.state
 
+import com.example.community_app.core.presentation.helpers.UiText
+
+data class UiControlState(
+  val isLoading: Boolean = false,
+  val errorMessage: UiText? = null,
+  val isFilterSheetVisible: Boolean = false
+)
