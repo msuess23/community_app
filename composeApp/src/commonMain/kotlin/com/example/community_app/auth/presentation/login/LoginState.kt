1531a1a41d5954d2d3617879261a80640a69ea67
msuess <moritz.suess@hof-university.de>
2025-12-24 00:31:35 +0100

    email & pw autoset for login

--- composeApp/src/commonMain/kotlin/com/example/community_app/auth/presentation/login/LoginState.kt
+++ composeApp/src/commonMain/kotlin/com/example/community_app/auth/presentation/login/LoginState.kt
@@ -3,8 +3,8 @@ package com.example.community_app.auth.presentation.login
 import com.example.community_app.core.presentation.helpers.UiText
 
 data class LoginState(
-  val email: String = "",
-  val password: String = "",
+  val email: String = "citizen1@demo.local", // TODO: for prod and prof
+  val password: String = "password!123", // TODO: for prod and prof
   val isPasswordVisible: Boolean = false,
   val isRememberMeChecked: Boolean = true,
   val isLoading: Boolean = false,
